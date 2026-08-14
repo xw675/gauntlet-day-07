b8101829c76ac253f431f7f50d3715a60ac2d650
Lew Xu Wei <xlew0002@student.monash.edu>
2026-08-15 00:35:59 +0800

    red: finds an added item and rejects a missing one

--- ex00/tests.cpp
+++ ex00/tests.cpp
@@ -6,3 +6,11 @@ TEST_CASE("adding an item raises the count") {
     inv.add("potion");
     CHECK(inv.count() == 1);
 }
+
+TEST_CASE("finds an added item and rejects a missing one") {
+    Inventory inv;
+    inv.add("potion");
+    inv.has("potion");
+    CHECK(inv.has("potion"));
+    CHECK_FALSE(inv.has("sword"));
+}
